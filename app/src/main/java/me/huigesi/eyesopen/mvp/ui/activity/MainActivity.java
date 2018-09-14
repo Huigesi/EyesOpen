09c002780e5edf39c9ee7dc3ff467aa8f5bd815f
Huigesi <791339970@qq.com>
2018-09-14 17:01:47 +0800

    QQ登录测试

--- app/src/main/java/me/huigesi/eyesopen/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/me/huigesi/eyesopen/mvp/ui/activity/MainActivity.java
@@ -10,18 +10,35 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.auth.QQToken;
+import com.tencent.connect.common.Constants;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.huigesi.eyesopen.R;
+import me.huigesi.eyesopen.app.utils.GlideUtils;
+import me.huigesi.eyesopen.app.utils.Resolution;
+import me.huigesi.eyesopen.app.utils.bigImgViewPager.glide.ImageLoader;
+import me.huigesi.eyesopen.app.utils.bigImgViewPager.tool.ToastUtil;
 import me.huigesi.eyesopen.di.component.DaggerMainComponent;
 import me.huigesi.eyesopen.di.module.MainModule;
 import me.huigesi.eyesopen.mvp.contract.MainContract;
@@ -55,6 +72,12 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     NavigationView mNav;
     @BindView(R.id.drawer_layout)
     DrawerLayout mDrawerLayout;
+    private Tencent mTencent;
+    private UserInfo mUserInfo;
+    private ImageView img_person;
+    private BaseUiListener mIUiListener;
+    private TextView mUserName;
+    private static final String APP_ID = "1105602574";
 
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
@@ -76,19 +99,47 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(getResources().getColor(R.color.colorTheme));
         }
+        View headerView = mNav.getHeaderView(0);
+        img_person = (ImageView) headerView.findViewById(R.id.person);
+        mUserName = (TextView) headerView.findViewById(R.id.tv_user_name);
+        mTencent = Tencent.createInstance(APP_ID, MainActivity.this.getApplicationContext());
         ArrayList<Fragment> mFragmentList = new ArrayList<>();
         mFragmentList.add(News163Fragment.newInstance());
         mFragmentList.add(WeiboFragment.newInstance());
         mFragmentList.add(NbaFragment.newInstance());
         MyFragmentAdapter adapter = new MyFragmentAdapter(getSupportFragmentManager(),
                 mFragmentList);
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+        mNav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_Setting:
+                        break;
+                    case R.id.menu_Login_QQ:
+                        mTencent.login(MainActivity.this, "all", new BaseUiListener());
+                        break;
+                    /*case R.id.menu_Login_WeiBo:
+                        UIUtils.startWeiBoLoginActivity(MainActivity.this);
+                        break;*/
+                    case R.id.menu_Clear:
+                        ImageLoader.cleanDiskCache(MainActivity.this);
+                        ToastUtil.getInstance()._short(MainActivity.this, "磁盘缓存已成功清除");
+                        break;
+                    case R.id.menu_AboutUs:
+                        break;
+                }
+                mDrawerLayout.closeDrawer(mNav);
+                return true;
+            }
+        });
         mVpContent.setAdapter(adapter);
         mVpContent.setOffscreenPageLimit(2);
         mVpContent.addOnPageChangeListener(this);
         mIvTitleMovie.setOnClickListener(this);
         mIvTitleNews.setOnClickListener(this);
         mIvTitleVideo.setOnClickListener(this);
-
+        mImgMenu.setOnClickListener(this);
         setCurrentItem(0);
     }
 
@@ -185,5 +236,85 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
                 }
                 break;
         }
+    }
+    /**
+     * 自定义监听器实现IUiListener接口后，需要实现的3个方法
+     * onComplete完成 onError错误 onCancel取消
+     */
+    private class BaseUiListener implements IUiListener {
+
+        @Override
+        public void onComplete(Object response) {
+            Toast.makeText(MainActivity.this, "授权成功", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "response:" + response);
+            JSONObject obj = (JSONObject) response;
+            try {
+                String openID = obj.getString("openid");
+                String accessToken = obj.getString("access_token");
+                String expires = obj.getString("expires_in");
+                mTencent.setOpenId(openID);
+                mTencent.setAccessToken(accessToken, expires);
+                QQToken qqToken = mTencent.getQQToken();
+                mUserInfo = new UserInfo(getApplicationContext(), qqToken);
+                mUserInfo.getUserInfo(new IUiListener() {
+                    @Override
+                    public void onComplete(Object response) {
+                        Log.e(TAG, "登录成功" + response.toString());
+                        try {
+                            int weight = Resolution.dipToPx(MainActivity.this, 72);
+                            GlideUtils.loadCircle(MainActivity.this,
+                                    ((JSONObject) response).getString("figureurl_1"),
+                                    img_person, weight, weight);
+                            mUserName.setText(((JSONObject) response).getString("nickname"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(UiError uiError) {
+                        Log.e(TAG, "登录失败" + uiError.toString());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.e(TAG, "登录取消");
+
+                    }
+                });
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onError(UiError uiError) {
+            Toast.makeText(MainActivity.this, "授权失败", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onCancel() {
+            Toast.makeText(MainActivity.this, "授权取消", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    /**
+     * 在调用Login的Activity或者Fragment中重写onActivityResult方法
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case Constants.REQUEST_LOGIN:
+                Tencent.onActivityResultData(requestCode, resultCode, data, mIUiListener);
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
